d1362f12f7498b4d2b845813da8d4cefa9b2c608
sharmavins23 <sharmavins23@gmail.com>
2022-06-01 14:40:07 -0400

    begun splitting of code files

---
+++ src/Apple.js
@@ -0,0 +1,23 @@
+class Apple {
+    constructor() {
+        var isTouching;
+
+        while (true) {
+            isTouching = false;
+            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size;
+            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size;
+
+            for (var i = 0; i < snake.tail.length; i++) {
+                if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
+                    isTouching = true;
+                }
+            }
+            this.color = "red"
+            this.size = snake.size;
+            if (!isTouching) {
+                break;
+            }
+
+        }
+    }
+}
